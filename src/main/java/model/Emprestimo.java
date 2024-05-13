010fc0669ac78c4f1de80a5004497d1400f2b6c1
thuys <thuys@10.150.102.145>
2024-05-13 20:55:46 -0300

    chore: remoção de comentario

--- src/main/java/model/Emprestimo.java
+++ src/main/java/model/Emprestimo.java
@@ -1,15 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author thuys
- */
 public class Emprestimo {
     
-    //oi thuysa
     
 }
