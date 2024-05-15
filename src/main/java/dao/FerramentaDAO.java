b771dac88a4ebae838f72c68abba3c90a1c3bb47
thuys <thuys@10.150.102.145>
2024-05-15 19:29:38 -0300

    chore: Arrumei a formatação da classe FerramentaDAO.

--- src/main/java/dao/FerramentaDAO.java
+++ src/main/java/dao/FerramentaDAO.java
@@ -8,39 +8,46 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import model.Ferramenta;
+
 /**
- * Classe responsável pela manipulação de dados das ferramentas no banco de dados.
+ * Classe responsável pela manipulação de dados das ferramentas no banco de
+ * dados.
+ *
  * @author Thuysa.
  */
-public class FerramentaDAO  {
-/**
- * Lista de ferramentas.
- */
-    public ArrayList<Ferramenta> minhaLista = new ArrayList<>();
-    
-    private ConexaoDataBaseDAO db;
+public class FerramentaDAO {
+
     /**
-     * Construtor da classe FerramentaDAO.
-     * Inicializa a conexão com o banco de dados.
+     * Lista de ferramentas.
      */
-    public FerramentaDAO (){
+    public ArrayList<Ferramenta> minhaLista = new ArrayList<>();
+
+    private ConexaoDataBaseDAO db;
+
+    /**
+     * Construtor da classe FerramentaDAO. Inicializa a conexão com o banco de
+     * dados.
+     */
+    public FerramentaDAO() {
         this.db = new ConexaoDataBaseDAO();
     }
-/**
- * Retorna a lista de ferramentas do banco de dados.
- * @return - Retorna a lista de ferramentas.
- */
+
+    /**
+     * Retorna a lista de ferramentas do banco de dados.
+     *
+     * @return - Retorna a lista de ferramentas.
+     */
     public ArrayList<Ferramenta> getMinhaLista() {
-/**
- * Limpa nosso ArrayList.
- */
-        minhaLista.clear(); 
+        /**
+         * Limpa nosso ArrayList.
+         */
+        minhaLista.clear();
 
         try {
             Statement stmt = db.getConexao().createStatement();
             ResultSet res = stmt.executeQuery("SELECT * FROM tb_ferramentas");
             while (res.next()) {
-                
+
                 int id = res.getInt("id");
                 String ferramenta = res.getString("ferramenta");
                 String marca = res.getString("marca");
@@ -57,17 +64,21 @@ public class FerramentaDAO  {
         }
         return minhaLista;
     }
-/**
- * Define a lista de ferramentas.
- * @param minhaLista - Lista de ferramentas a ser definida.
- */
+
+    /**
+     * Define a lista de ferramentas.
+     *
+     * @param minhaLista - Lista de ferramentas a ser definida.
+     */
     public void setMinhaLista(ArrayList<Ferramenta> minhaLista) {
         this.minhaLista = minhaLista;
     }
-/**
- * Retorna o maior ID de ferramenta presente no banco de dados.
- * @return - Retorna o maior ID de ferramenta.
- */
+
+    /**
+     * Retorna o maior ID de ferramenta presente no banco de dados.
+     *
+     * @return - Retorna o maior ID de ferramenta.
+     */
     public int maiorID() {
         int maiorID = 0;
         try {
@@ -81,11 +92,13 @@ public class FerramentaDAO  {
         }
         return maiorID;
     }
-  
+
     /**
      * Insere uma nova ferramenta no banco de dados.
+     *
      * @param objeto - A ferramenta a ser inserida.
-     * @return - Verdadeiro se a inserção foi bem-sucedida, falso caso contrário.
+     * @return - Verdadeiro se a inserção foi bem-sucedida, falso caso
+     * contrário.
      */
     public boolean insertFerramentaBD(Ferramenta objeto) {
         String sql = "INSERT INTO tb_ferramentas(id,ferramenta,marca,preco) VALUES(?,?,?,?)";
@@ -107,11 +120,12 @@ public class FerramentaDAO  {
         }
     }
 
-/**
- * Remove uma ferramenta do banco de dados com base em seu ID.
- * @param id - O ID da ferramenta a ser removida.
- * @return - Verdadeiro se a remoção foi bem-sucedida, falso caso contrário.
- */
+    /**
+     * Remove uma ferramenta do banco de dados com base em seu ID.
+     *
+     * @param id - O ID da ferramenta a ser removida.
+     * @return - Verdadeiro se a remoção foi bem-sucedida, falso caso contrário.
+     */
     public boolean deleteFerramentaBD(int id) {
         try {
             Statement stmt = db.getConexao().createStatement();
@@ -124,11 +138,13 @@ public class FerramentaDAO  {
         return true;
     }
 
-/**
- * Atualiza os dados de uma ferramenta no banco de dados.
- * @param objeto - A ferramenta com os dados atualizados.
- * @return - Verdadeiro se a atualização foi bem-sucedida, falso caso contrário.
- */
+    /**
+     * Atualiza os dados de uma ferramenta no banco de dados.
+     *
+     * @param objeto - A ferramenta com os dados atualizados.
+     * @return - Verdadeiro se a atualização foi bem-sucedida, falso caso
+     * contrário.
+     */
     public boolean updateFerramentaBD(Ferramenta objeto) {
 
         String sql = "UPDATE tb_ferramentas set ferramenta = ? ,marca = ? ,preco = ? WHERE id = ?";
@@ -152,21 +168,21 @@ public class FerramentaDAO  {
         }
     }
 
-/**
- * Carrega uma ferramenta do banco de dados com base em seu ID.
- * @param id - O ID da ferramenta a ser carregada.
- * @return - Retorna a ferramenta carregada do banco de dados.
- */
+    /**
+     * Carrega uma ferramenta do banco de dados com base em seu ID.
+     *
+     * @param id - O ID da ferramenta a ser carregada.
+     * @return - Retorna a ferramenta carregada do banco de dados.
+     */
     public Ferramenta carregaFerramenta(int id) {
         Ferramenta objeto = new Ferramenta();
         objeto.setId(id);
         try {
             Statement stmt = db.getConexao().createStatement();
-            
+
             ResultSet res = stmt.executeQuery("SELECT * FROM tb_ferramentas WHERE id = " + id);
             res.next();
-            
-            
+
             objeto.setFerramenta(res.getString("ferramenta"));
             objeto.setMarca(res.getString("marca"));
             objeto.setPreco(res.getDouble("preco"));
