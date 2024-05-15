58aa62c5b94a8cb06c7dcea4580c7a5802252a1b
thuys <thuys@10.150.102.145>
2024-05-15 20:55:11 -0300

    docs: Adicionei documentos do código da classe Mensagem.

--- src/main/java/view/Mensagem.java
+++ src/main/java/view/Mensagem.java
@@ -1,7 +1,14 @@
 package view;
-
+/**
+ * Classe de exceção personalizada para mensagens específicas.
+ * Permite a criação de exceções personalizadas com mensagens específicas.
+ * Esta classe é utilizada para lançar mensagens de erro em situações específicas.
+ */
 public class Mensagem extends Exception {
-
+/**
+ * Construtor da classe Mensagem.
+ * @param mensagem - A mensagem de erro a ser exibida.
+ */
     public Mensagem(String mensagem){
         super(mensagem);
     }
