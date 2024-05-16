59bee6172abd7bbf463c6013d04b2980f09b38f0
thuys <thuys@10.150.102.145>
2024-05-15 21:12:08 -0300

    docs: Adicionei documentos do código da classe Principal.

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,9 +1,16 @@
 package principal;
 
 import view.FrmMenuPrincipal;
-
+/**
+ * Classe principal responsável por iniciar a aplicação.
+ * A classe Principal contém o método main que é o ponto de entrada da aplicação.
+ */
 public class Principal {
-
+/**
+ * Método main que inicia a aplicação.
+ * Cria uma instância da classe FrmMenuPrincipal e a torna visível.
+ * @param args - Os argumentos da linha de comando (não utilizados neste caso).
+ */
     public static void main(String[] args) {
 
         FrmMenuPrincipal objetotela = new FrmMenuPrincipal();
