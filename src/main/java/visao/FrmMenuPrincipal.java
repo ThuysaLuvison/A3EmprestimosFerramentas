bebad0babb2a7b5b69f4b5380008a6c524832a09
thuys <thuys@10.150.102.145>
2024-05-20 19:37:53 -0300

    chore: Arrumei a formatação da classe FrmMenuPrincipal

--- src/main/java/visao/FrmMenuPrincipal.java
+++ src/main/java/visao/FrmMenuPrincipal.java
@@ -1,27 +1,28 @@
 package visao;
 
- /**
- * Classe responsável por fornecer o menu principal da aplicação.
- * Permite acessar diferentes funcionalidades, como cadastro de ferramentas, cadastro de amigos, visualização de relatórios e opção de sair.
- * Esta classe utiliza outras classes para exibir as funcionalidades correspondentes.
- * O menu é composto por opções de "Opções" e "Sobre".
+/**
+ * Classe responsável por fornecer o menu principal da aplicação. Permite
+ * acessar diferentes funcionalidades, como cadastro de ferramentas, cadastro de
+ * amigos, visualização de relatórios e opção de sair. Esta classe utiliza
+ * outras classes para exibir as funcionalidades correspondentes. O menu é
+ * composto por opções de "Opções" e "Sobre".
  *
  * @author Thuysa
  */
 public class FrmMenuPrincipal extends javax.swing.JFrame {
 
     /**
-     * Cria uma nova instância do formulário FrmMenuPrincipal.
-     * Inicializa os componentes da interface gráfica.
+     * Cria uma nova instância do formulário FrmMenuPrincipal. Inicializa os
+     * componentes da interface gráfica.
      */
     public FrmMenuPrincipal() {
         initComponents();
     }
 
-     /**
-     * Este método é chamado a partir do construtor para inicializar o formulário.
-     * AVISO: Não modifique este código. O conteúdo deste método é sempre
-     * regenerado pelo editor de formulários.
+    /**
+     * Este método é chamado a partir do construtor para inicializar o
+     * formulário. AVISO: Não modifique este código. O conteúdo deste método é
+     * sempre regenerado pelo editor de formulários.
      */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -107,18 +108,20 @@ public class FrmMenuPrincipal extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 /**
- * Abre a janela de cadastro de ferramentas.
- * @param evt - O evento de clique do item de menu.
- */
+     * Abre a janela de cadastro de ferramentas.
+     *
+     * @param evt - O evento de clique do item de menu.
+     */
     private void jMenuItemFerramentasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemFerramentasActionPerformed
-       
+
         FrmCadastroFerramentas objeto = new FrmCadastroFerramentas();
         objeto.setVisible(true);
     }//GEN-LAST:event_jMenuItemFerramentasActionPerformed
-/**
- * Abre a janela de cadastro de amigos.
- * @param evt - O evento de clique do item de menu.
- */
+    /**
+     * Abre a janela de cadastro de amigos.
+     *
+     * @param evt - O evento de clique do item de menu.
+     */
     private void jMenuItemCadastroAmigosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemCadastroAmigosActionPerformed
         /**
          * Abre a janela de cadastro de amigos.
@@ -126,37 +129,37 @@ public class FrmMenuPrincipal extends javax.swing.JFrame {
         FrmCadastroAmigos objeto = new FrmCadastroAmigos();
         objeto.setVisible(true);
     }//GEN-LAST:event_jMenuItemCadastroAmigosActionPerformed
-/**
- * Fecha a aplicação.
- * @param evt - O evento de clique do item de menu.
- */
+    /**
+     * Fecha a aplicação.
+     *
+     * @param evt - O evento de clique do item de menu.
+     */
     private void jMenuItemSairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemSairActionPerformed
-       
-       /**
-        * Fecha a aplicação.
-        */
+
+        /**
+         * Fecha a aplicação.
+         */
         System.exit(0);
     }//GEN-LAST:event_jMenuItemSairActionPerformed
-/**
- * Abre a janela de relatórios.
- * @param evt - O evento de clique do item de menu.
- */
+    /**
+     * Abre a janela de relatórios.
+     *
+     * @param evt - O evento de clique do item de menu.
+     */
     private void jMenuItemRelatorioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemRelatorioActionPerformed
-       /**
-        * Abre a janela de relatórios.
-        */
+        /**
+         * Abre a janela de relatórios.
+         */
         FrmRelatorio objeto = new FrmRelatorio();
         objeto.setVisible(true);
     }//GEN-LAST:event_jMenuItemRelatorioActionPerformed
 
-   
     /**
-     * 
      * @param args - Argumentos da linha de comando.
      */
     public static void main(String args[]) {
-       /**
-         * Defina a aparência do Nimbus. 
+        /**
+         * Defina a aparência do Nimbus.
          */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -180,7 +183,6 @@ public class FrmMenuPrincipal extends javax.swing.JFrame {
         }
         //</editor-fold>
 
-        
         /**
          * Cria e exibe o formulário.
          */
